4cf7a8f1dfd355bc6ebeba06dca1c66e165e7b4a
Sean Chen <skypemaster007@gmail.com>
2020-07-17 15:28:53 -0700

    Implement rebalancing

--- src/lib.rs
+++ src/lib.rs
@@ -4,17 +4,20 @@ extern crate quickcheck;
 #[macro_use(quickcheck)]
 extern crate quickcheck_macros;
 
+use std::cmp;
+use std::mem;
 use std::cmp::{Ord, Ordering};
 use std::iter::FromIterator;
 
-#[derive(Debug)]
-struct AvlNode<T: Ord> {
-    value: T,
-    left: AvlTree<T>,
-    right: AvlTree<T>,
+#[derive(Debug, PartialEq)]
+pub struct AvlNode<T: Ord> {
+    pub value: T,
+    pub left: AvlTree<T>,
+    pub right: AvlTree<T>,
+    pub height: usize,
 }
 
-type AvlTree<T> = Option<Box<AvlNode<T>>>;
+pub type AvlTree<T> = Option<Box<AvlNode<T>>>;
 
 struct AvlTreeSet<T: Ord> {
     root: AvlTree<T>,
@@ -24,6 +27,115 @@ struct AvlTreeSet<T: Ord> {
 struct AvlTreeSetIter<'a, T: Ord> {
     prev_nodes: Vec<&'a AvlNode<T>>,
     current_tree: &'a AvlTree<T>,
+}
+
+impl<'a, T: 'a + Ord> AvlNode<T> {
+    pub fn left_height(&self) -> usize {
+        self.left.as_ref().map_or(0, |left| left.height)
+    }
+
+    pub fn right_height(&self) -> usize {
+        self.right.as_ref().map_or(0, |right| right.height)
+    }
+    
+    pub fn update_height(&mut self) {
+        self.height = cmp::max(self.left_height(), self.right_height()) + 1;
+    }
+
+    pub fn balance_factor(&self) -> i8 {
+        let left_height = self.left_height();
+        let right_height = self.right_height();
+
+        if left_height >= right_height {
+            (left_height - right_height) as i8
+        } else {
+            -((right_height - left_height) as i8)
+        }
+    }
+
+    pub fn rotate_left(&mut self) -> bool {
+        if self.right.is_none() { 
+            return false;
+        }
+
+        let right_node = self.right.as_mut().unwrap();
+        let right_left_tree = right_node.left.take();
+        let right_right_tree = right_node.right.take();
+
+        let mut new_left_tree = mem::replace(&mut self.right, right_right_tree);
+        mem::swap(&mut self.value, &mut new_left_tree.as_mut().unwrap().value);
+
+        let left_tree = self.left.take();
+        let new_left_node = new_left_tree.as_mut().unwrap();
+
+        new_left_node.right = right_left_tree;
+        new_left_node.left = left_tree;
+        self.left = new_left_tree;
+
+        if let Some(node) = self.left.as_mut() {
+            node.update_height();
+        }
+
+        self.update_height();
+
+        true
+    }
+
+    pub fn rotate_right(&mut self) -> bool {
+        if self.left.is_none() {
+            return false;
+        }
+
+        let left_node = self.left.as_mut().unwrap();
+        let left_left_tree = left_node.left.take();
+        let left_right_tree = left_node.right.take();
+
+        let mut new_right_tree = mem::replace(&mut self.left, left_left_tree);
+        mem::swap(&mut self.value, &mut new_right_tree.as_mut().unwrap().value);
+
+        let right_tree = self.right.take();
+        let new_right_node = new_right_tree.as_mut().unwrap();
+
+        new_right_node.left = left_right_tree;
+        new_right_node.right = right_tree;
+        self.right = new_right_tree;
+
+        if let Some(node) = self.right.as_mut() {
+            node.update_height();
+        }
+
+        self.update_height();
+
+        true
+    }
+
+    pub fn rebalance(&mut self) -> bool {
+        match self.balance_factor() {
+            -2 => {
+                let right_node = self.right.as_mut().unwrap();
+
+                if right_node.balance_factor() == 1 {
+                    right_node.rotate_right();
+                }
+
+                self.rotate_left();
+
+                true
+            },
+            2 => {
+                let left_node = self.left.as_mut().unwrap();
+
+                if left_node.balance_factor() == -1 {
+                    left_node.rotate_left();
+                }
+
+                self.rotate_right();
+                
+                true
+            },
+            _ => false,
+        }
+    }
 }
 
 impl<T: Ord> AvlTreeSet<T> {
@@ -46,6 +158,7 @@ impl<T: Ord> AvlTreeSet<T> {
             value,
             left: None,
             right: None,
+            height: 0,
         }));
 
         true
